49c2c3994ab9a7fc4529ff29342299a147e9d3fa
hsuan1117 <34859514+hsuan1117@users.noreply.github.com>
2019-09-07 15:35:57 +0800

    Create tainanForum-post.js

---
+++ tainanForum-post.js
@@ -0,0 +1,62 @@
+var editor;
+
+function publish() {
+  var insertHTML = `<div class='date-outer'><div class='post' style='margin: 15px 0;'></div></div>`;
+  $(".date-outer").remove();
+  $(".blog-posts").append(insertHTML);
+  $(".post").eq(0).load("/p/publish.html .post-body", function() {
+    $("textarea[name='CKeditor']").after(`<div id='editorGif' style='margin: 20px auto;text-align: center;'><span style='vertical-align: middle;'>Please Wait,editor is loading.....</span></div>`);
+    $.getScript("https://cdn.ckeditor.com/ckeditor5/12.4.0/classic/ckeditor.js", function() {
+      ClassicEditor.create(
+        document.querySelector('#CKeditor')
+      ).then((editorObject) => {
+        editor = editorObject
+        $("#CKeditor_is_loading").hide()
+      }).catch((error) => {
+        alert("CKeditor錯誤:" + String(error))
+      })
+      $("#submitPost").click(function() {
+        submit()
+      });
+    })
+  })
+}
+
+function submit() {
+  var content = editor.getData(),
+    title = $("#inputPostTitle").val(),
+    labels = [$("#selectLabel").val()],
+    publishTime = new Date().toISOString(),
+    name = window.AuthedUser.displayName;
+
+  if (!title) {
+    alert("請輸入文章標題！");
+    return
+  }
+  if (content.length < 10) {
+    alert("字數過少，不合格");
+    return
+  }
+
+  if (content.indexOf("<img") > 0) {
+    content = (
+      content.substring(0, content.indexOf("<img")) +
+      "<!--more-->" +
+      content.substring(content.indexOf("<img"))
+    )
+  }
+
+  $("#submitPost").after("<div id='submitGif' style='margin: 20px auto;text-align: center;'><img src='https://lh5.googleusercontent.com/-EyVZ0f8J0qQ/UCeEG7aa8nI/AAAAAAAADtY/9sXw53XkYXM/s512/indicator-light.gif' style='vertical-align: middle;' /><span style='vertical-align: middle;'> 傳送中...</span></div>")
+  $.post('https://script.google.com/macros/s/AKfycbwq_YryQm6oviwghUxlDNxtHKfNuZmlRfBZ0YHLJ0eVhX6tGTuI/exec', {
+    title: title,
+    time: publishTime,
+    content: content,
+    labels: labels,
+    name: name
+  }).then(function(response) {
+    alert(response.status);
+    location.href=response.link;
+  }).error(function(error) {
+    alert(String(error))
+  })
+}
